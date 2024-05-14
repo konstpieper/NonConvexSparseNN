48cd44ee43c79f386a43d13ae80f133e0973c2d1
Konstantin Pieper <pieperk@ornl.gov>
2024-05-14 14:30:47 -0400

    add test case 4 from the paper

--- paper_test_4.m
+++ paper_test_4.m
@@ -10,45 +10,54 @@ if is_octave
   pkg load optim;
 end
 
-%f_d = @(x) sqrt(sum((x+.1).^2,1));
-%f_d = @(x) cos(10*x(1,:).*x(2,:)) .* exp(-sum(4*x.^2,1)/2);
-%f_d = @(x) max(0, 1 - 2*max(abs(x(1,:)), abs(x(2,:))));
-%f_d = @(x) exp(- sum((3*x).^2, 1) / 2);
-
-%kappa = 1000;
-%softmax = @(a,b) (1/kappa) * (kappa*max(a,b) + log(exp(kappa*(a-max(a,b))) + exp(kappa*(b-max(a,b)))));
-%f_d = @(x) softmax(0,softmax(x(1,:),x(2,:)));
-
-%huber = @(t) (t <= 1/2).*(1/2).*t.^2 + (t > 1/2).*(1/2).*(t-1/4);
-%f_d = @(x) huber(sqrt(sum((x+.1).^2, 1)));
-
 f_d = @(x) sum(x(2:end,:).^2./(x(1,:)+1.05), 1) + 2*(x(1,:)+1.05).^4;
-
-%f_d = @(x) (x(1,:) >= 0) | (x(2,:) >= 0);
-
-%f_d = @(x) sqrt(sum(x.^2, 1));
-%f_d = @(x) sqrt(x(1,:).^2+x(2,:).^2);
 y_d = f_d(p.xhat)';
 
 alg_opts = struct();
 alg_opts.max_step = 10;
 alg_opts.plot_every = 0;
+alg_opts.plot_final = 0;
 alg_opts.sparsification = false;
 alg_opts.TOL = 1e-6;
 
 alpha = .01;
+alphas = alpha*[1, 1/4, 1/16, 1/64];
 
-%gamma = 0;
-%phi = p.Phi(p, gamma);
+gamma = 0;
+phi = p.Phi(p, gamma);
 
-%[u_opt_1, alg_out_1] = PDAPmultisemidiscrete(p, y_d, alpha, phi, alg_opts);
+u_l1 = cell(length(alphas),1);
+alg_out_1 = cell(length(alphas),1);
+Nnodes_l1 = cell(length(alphas),1);
+l2_err_l1 = cell(length(alphas),1);
+
+uinit = p.u_zero;
+
+for n = 1:length(alphas)
+  alpha = alphas(n);
+
+  alg_opts.u0 = uinit;
+  [u_l1{n}, alg_out_1{n}] = PDAPmultisemidiscrete(p, y_d, alpha, phi, alg_opts);
+
+  uinit = u_l1{n};
+
+  figure(1);
+  p.plot_adjoint(p, u_l1{n}, p.obj.dF(p.K(p, p.xhat, u_l1{n})-y_d), alpha)
+  %matlab2tikz('paper_test_4/l1_adjoint.tikz')
+  figure(2);
+  p.plot_forward(p, u_l1{n}, y_d)
+  %matlab2tikz('paper_test_4/l1_forward.tikz')
+  drawnow;
+
+  Nnodes_l1{n} = length(u_l1{n}.x)
+  l2_err_l1{n} = sqrt(2*p.obj.F(p.K(p, p.xhat, u_l1{n})-y_d))
+end
 
 gamma = 5;
 phi = p.Phi(p, gamma);
 
 alg_opts.optimize_x = true;
-
-alphas = alpha*[1,1/4,1/16,1/64];
+alg_opts.sparsification = true;
 
 u_opt = cell(length(alphas),1);
 alg_out = cell(length(alphas),1);
@@ -59,20 +68,20 @@ uinit = p.u_zero;
 
 for n = 1:length(alphas)
   alpha = alphas(n);
-  
+
   alg_opts.u0 = uinit;
   [u_opt{n}, alg_out{n}] = PDAPmultisemidiscrete(p, y_d, alpha, phi, alg_opts);
 
   uinit = u_opt{n};
-  
+
   figure(3);
   p.plot_adjoint(p, u_opt{n}, p.obj.dF(p.K(p, p.xhat, u_opt{n})-y_d), alpha)
-  %matlab2tikz('paper_test_3/phi_adjoint.tikz')
+  %matlab2tikz('paper_test_4/phi_adjoint.tikz')
   figure(4);
   p.plot_forward(p, u_opt{n}, y_d)
-  %matlab2tikz('paper_test_3/phi_forward.tikz')
+  %matlab2tikz('paper_test_4/phi_forward.tikz')
   drawnow;
-  
+
   Nnodes_phi{n} = length(u_opt{n}.x)
   l2_err_phi{n} = sqrt(2*p.obj.F(p.K(p, p.xhat, u_opt{n})-y_d))
 end
